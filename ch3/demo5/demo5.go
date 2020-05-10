6902221838b4f67f0a38a74a654dcbc30e15f91c
Ami.Tsai <ami@ami.com>
2020-05-10 15:39:11 +0800

    數組,切片使用方式

--- ch3/demo5/demo5.go
+++ ch3/demo5/demo5.go
@@ -8,15 +8,28 @@ var container = []string{"zero", "one", "two"}
 //透過斷言表達式 interface{}(container).([]string)
 func main() {
 
+	//區域變數覆蓋包級變數
 	container := map[int]string{0: "zero", 1: "one", 2: "two"}
 
 	fmt.Printf("the emelent is %q.\n", container[1])
-	//空介面轉型物件,介面可判斷類型 .(類型)
-	_, ok := interface{}(container).([]string)
 
-	if ok {
-		fmt.Println("container is slice")
-	} else {
-		fmt.Println("container is map")
+	//空介面轉型物件,介面可判斷類型 .(類型)
+	//assertType 若不正確,value = nil
+	//若正確,則value會為正確的值
+	//型別判斷false
+	value, ok := interface{}(container).([]string)
+	fmt.Println(value)
+	fmt.Println(ok)
+
+	//型別判斷true
+	value1, ok1 := interface{}(container).(map[int]string)
+	fmt.Println(value1)
+	fmt.Println(ok1)
+
+	//型別判斷t中間擺入表達式
+	var value2, ok2 = interface{}(1 + 2).(int)
+	if ok2 {
+		fmt.Println(value2)
 	}
+
 }
---
+++ ch4/demo6/demo6.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+//數組,切片 array slice
+func main() {
+	//宣告數組1
+	var array1 [3]int
+	fmt.Println(array1)
+	array1[0] = 1
+
+	//宣告數組2
+	var array2 = [3]int{0, 1, 2}
+	fmt.Println(array2)
+
+	//宣告切片1
+	var slice1 []int
+	slice1 = append(slice1, 1)
+	fmt.Println(slice1[0])
+
+	//宣告切片2
+	//先宣告array,在從array中提取切片
+	var sliceArray [10]int
+	var slice2 = sliceArray[0:5]
+	fmt.Println(slice2)
+
+	//宣告切片3
+	//透過make製作切片
+	var slice3 = make([]int, 3, 10)
+	//切片長度 --3
+	fmt.Println(len(slice3))
+	//切片容量 --10
+	fmt.Println(cap(slice3))
+	fmt.Println(slice3)
+
+	//塞入20個元素
+	for i := 0; i < 20; i++ {
+		slice3 = append(slice3, i)
+	}
+
+	//切片長度為23
+	fmt.Println(len(slice3))
+	//切片容量為40,當切片到其容量上限時,會將其底層的array複製一倍容量,所以目前容量上限若為50,當數量要往51增加時,容量會擴充為100
+	//當數量為100,繼續增加時,會擴充為200
+	fmt.Println(cap(slice3))
+	fmt.Println(slice3)
+
+	//golang 基礎類型,struct array = 值類型(傳值)
+	//slice map chan function 為引用,存的為指標的value
+}
